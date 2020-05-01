5cd1daebf84dbf9dff145e33d51e375dfdf7151d
Monish259 <46106169+Monish259@users.noreply.github.com>
2020-05-01 14:45:59 +0530

    changes in SocialLogin file 2nd

--- src/User/SocialLogin.js
+++ src/User/SocialLogin.js
@@ -41,7 +41,7 @@ class SocialLogin extends Component {
         return (
             <div className="container">
                 <GoogleLogin
-                    clientId="381845043542-idbi7kqc2652h5jbrean684p2vfn7lav.apps.googleusercontent.com"
+                    clientId="381845043542-u797im493juhal4vvnt8lke6mo2uj8g4.apps.googleusercontent.com"
                     buttonText="Login with Google"
                     onSuccess={this.responseGoogle}
                     onFailure={this.responseGoogle}
